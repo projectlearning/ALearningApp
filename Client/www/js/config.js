311a62622b66b3a8e49ba578816dfc84984ceeca
lestermbaf <liwaiwailiweiwei@gmail.com>
2015-08-20 23:32:42 +0800

    add config.js on behalf of wangxueyi

---
+++ Client/www/js/config.js
@@ -0,0 +1,12 @@
+angular.module('alearn.config',[])
+
+.constant('config',{
+  /*time: 2015-08-16
+  *author: ant
+  *content: platform config
+  */
+  platFormConfig: {
+    host: '',
+    verision: '0.0.0.0',
+  },
+})
