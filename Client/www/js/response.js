6725a1bec95a06451ccbe24482dc36d9fe22efa8
wangxueyi <wangxueyi_ant@163.com>
2015-10-15 11:20:56 +0800

    Response Config File

---
+++ Client/www/js/response.js
@@ -0,0 +1,11 @@
+var responseCode = {
+	Login_Fail: "登录失败",
+  	Phone_Not_Empty: "请输入手机号",
+  	Phone_Not_Right: "手机格式不正确",
+  	Password_Not_Empty: "请输入密码",
+  	Register_Wrong_Verification_Code: "验证码不正确",
+  	Verification_Code_Not_Empty: "请输入验证码",
+  	Logining: "正在登录...",
+  	Registering: "正在注册..",
+  	Register_Fail: "注册失败",
+  }
